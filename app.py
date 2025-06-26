344ef9cf2d1bc6c8cfe7430b931f0e38119a6fea
Moonis11 <141505520+Moonis11@users.noreply.github.com>
2025-06-26 16:10:13 +0500

    Update app.py

--- app.py
+++ app.py
@@ -152,7 +152,7 @@ if uploaded_file:
     col1, col2 = st.columns([1, 2], gap="large")
 
     with col1:
-        st.image(image, caption="📸", use_container_width=True)
+        st.image(image, caption="📸")
 
     with col2:
         with st.spinner(translations["detecting"][lang]):
